f841103524c4a8e69aa1a6181768d0d54b9497d4
Taghreed <taghreedoyoun@gmail.com>
2025-12-20 23:41:14 +0200

    Final: Clean structure with redirected frontend and organized docs

--- api.js
+++
@@ -1,9 +0,0 @@
-const API_BASE = "http://127.0.0.1:8000";
-
-function getAuthHeaders() {
-    const token = localStorage.getItem("access_token");
-    return {
-        "Content-Type": "application/json",
-        "Authorization": token ? `Bearer ${token}` : ""
-    };
-}
